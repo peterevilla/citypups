0dd02438c07ce9c32c2b3a957afedfad3988731d
Pedro Revilla <peterevilla@gmail.com>
2020-10-02 21:39:31 -0500

    styling

--- src/components/Find/DogCard.js
+++ src/components/Find/DogCard.js
@@ -1,30 +1,26 @@
 import React from "react";
-import DogModal from "./DogModal"
+import DogModal from "./DogModal";
 
-const DogCard = ({dog}) => {
- 
+const DogCard = ({ dog }) => {
   return (
-    <div className=" h-auto max-w-lg  flex font-sans lg:flex-row flex-col shadow-md rounded-md my-8 p-2
+    <div
+      className=" h-auto max-w-lg  flex font-sans lg:flex-row flex-col shadow-md rounded-md my-8 p-2
     transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-100 hover:bg-gray-200
-    ">
+    "
+    >
       <div className="flex flex-col">
-  <div className="font-bold py-6">{dog.shelter_name}</div>
-        <img
-          className="w-full"
-          src={`${dog.image_url}`}
-          alt="dog"
-        />
+        <div className="font-bold py-6">{dog.shelter_name}</div>
+        <img className="w-full" src={`${dog.image_url}`} alt="dog" />
       </div>
 
       <div className="flex flex-col lg:py-6">
-        <div className=" lg:p-20 flex flex-col justify-around">
-  <span>{dog.dog_name}</span>
-          <div >
-      <DogModal dog={dog}/>
-      </div>
+        <div className=" lg:px-16 py-20 flex flex-col justify-around">
+          <span>{dog.dog_name}</span>
+          <div>
+            <DogModal dog={dog} />
+          </div>
         </div>
       </div>
-      
     </div>
   );
 };
--- src/components/Find/Find.js
+++ src/components/Find/Find.js
@@ -19,9 +19,9 @@ const Find = () => {
     return (
         <div >
              <Header2/>
-      
-        <div className=" h-screen bg-white flex flex-col  justify-start p-2 lg:p-48 ">
-
+     
+        <div className=" h-screen bg-white flex flex-col  justify-start p-2 lg:px-56 lg:py-10 py-8 ">
+        <p className="font-chewy text-green-900 text-xl -mt-8">Dogs Availables in Chicago,IL</p>
             {dogs.map(dog => (
                 <DogCard dog={dog} key={dog.id}/>
             ))}
